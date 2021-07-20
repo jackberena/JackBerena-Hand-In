87a8a39088197fe7bfba567052f869b0f5c0e3ee
jackberena <87047048+jackberena@users.noreply.github.com>
2021-07-20 14:04:18 -0700

    hand in

--- Rock Paper Scissors/src/rpc.java
+++ Rock Paper Scissors/src/rpc.java
@@ -13,84 +13,122 @@ public class rpc {
         Scanner sc = new Scanner (System.in);
         Random rand = new Random();
         int num;
+        // score variable
+        int wins = 0;
+        int losses = 0;
 
-        String userChoice = "";
-        String computerChoice = "";
 
-        System.out.println("Welcome to Rock, Paper, Scissors!");
-        System.out.println("Choose R)ock, P) aper, or S) cissors.");
-
-        userChoice = sc.nextLine();
-        //computer choice
-        num =rand.nextInt(3);
-        if ((num == 0))
-        {
-            computerChoice = "R";
-        }
-        if ((num == 1))
-        {
-            computerChoice = "S";
-        }
-        if ((num == 2))
-        {
-            computerChoice = "P";
-        }
+        //loop
+        while (true){String userChoice = "";
+            String computerChoice = "";
 
 
 
-        //Print computer choice
-        if (computerChoice.equals("S"))
-        {
-            System.out.println("Computer chose scissors");
-
-        }
-        if(computerChoice.equals("R"))
-        {
-            System.out.println("Computer chose Rock");
-        }
-        if (computerChoice.equals("P"))
-        {
-            System.out.println("Computer chose Paper");
-        }
-
-        //Determine Winner
-        if (userChoice.equals("R") && computerChoice.equals("S"))
-        {
-            System.out.println("U win!");
-        }
-        if (userChoice.equals("R") && computerChoice.equals("P"))
-        {
-            System.out.println("U lose!");
-        }
-        if (userChoice.equals("R") && computerChoice.equals("R"))
-        {
-            System.out.println("Tie!");
-        }
-        if (userChoice.equals("P") && computerChoice.equals("S"))
-        {
-            System.out.println("U lose!");
-        }
-        if (userChoice.equals("P") && computerChoice.equals("R"))
-        {
-            System.out.println("U win!");
-        }
-        if (userChoice.equals("P") && computerChoice.equals("P"))
-        {
-            System.out.println("Tie!");
-        }
-        if (userChoice.equals("S") && computerChoice.equals("S"))
-        {
-            System.out.println("Tie!");
-        }
-        if (userChoice.equals("S") && computerChoice.equals("P"))
-        {
-            System.out.println("U win!");
-        }
-        if (userChoice.equals("S") && computerChoice.equals("R"))
-        {
-            System.out.println("U lose!");
-        }
-    }
 
 
-}
+            System.out.println("Welcome to Rock, Paper, Scissors!");
+            System.out.println("Choose R)ock, P) aper, or S) cissors.");
+            System.out.println("Type quit to leave");
+
+
+
+            userChoice = sc.nextLine();
+            if (userChoice.equals("quit")){
+                break;
+            }
+
+            //computer choice
+            num =rand.nextInt(3);
+            if ((num == 0))
+            {
+                computerChoice = "R";
+            }
+            if ((num == 1))
+            {
+                computerChoice = "S";
+            }
+            if ((num == 2))
+            {
+                computerChoice = "P";
+            }
+            if (userChoice.equals("quit")){
+                break;
+            }
+
+
+
+            //Print computer choice
+            if (computerChoice.equals("S"))
+            {
+                System.out.println("Computer chose scissors");
+
+            }
+            if(computerChoice.equals("R"))
+            {
+                System.out.println("Computer chose Rock");
+            }
+            if (computerChoice.equals("P"))
+            {
+                System.out.println("Computer chose Paper");
+            }
+
+            //Determine Winner
+            if (userChoice.equals("R") && computerChoice.equals("S"))
+            {
+                System.out.println("U win!");
+                wins++;
+            }
+            if (userChoice.equals("R") && computerChoice.equals("P"))
+            {
+                System.out.println("U lose!");
+                losses++;
+            }
+            if (userChoice.equals("R") && computerChoice.equals("R"))
+            {
+                System.out.println("Tie!");
+            }
+            if (userChoice.equals("P") && computerChoice.equals("S"))
+            {
+                System.out.println("U lose!");
+                losses++;
+            }
+            if (userChoice.equals("P") && computerChoice.equals("R"))
+            {
+                System.out.println("U win!");
+                wins++;
+            }
+            if (userChoice.equals("P") && computerChoice.equals("P"))
+            {
+                System.out.println("Tie!");
+            }
+            if (userChoice.equals("S") && computerChoice.equals("S"))
+            {
+                System.out.println("Tie!");
+            }
+            if (userChoice.equals("S") && computerChoice.equals("P"))
+            {
+                System.out.println("U win!");
+                wins++;
+            }
+            if (userChoice.equals("S") && computerChoice.equals("R"))
+            {
+                System.out.println("U lose!");
+                losses++;
+            }
+            //print wins and losses
+            System.out.println("Games won " + wins );
+            System.out.println("Games lost " + losses );
+            System.out.println('\t');
+
+       }
+        }}
+
+
+
+
+
+
+
+
+
+
